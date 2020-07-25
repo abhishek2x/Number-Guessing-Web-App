34313eb2068fe7f01c55f96b0fdbae441e9be1c2
Abhishek Srivastava <=>
2020-07-26 03:32:18 +0530

    Updates

---
+++ precache-manifest.94024d00238bee82d740622f2429dd31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113e5952192f43954b8b09c88eeb84cf",
+    "url": "/abhishek2x/Number-Guessing-Web-App/index.html"
+  },
+  {
+    "revision": "44c722031fd936998174",
+    "url": "/abhishek2x/Number-Guessing-Web-App/static/js/2.b55185c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/abhishek2x/Number-Guessing-Web-App/static/js/2.b55185c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53218b3a746b95088fa6",
+    "url": "/abhishek2x/Number-Guessing-Web-App/static/js/main.954c1131.chunk.js"
+  },
+  {
+    "revision": "16f6d6c9229592b4781d",
+    "url": "/abhishek2x/Number-Guessing-Web-App/static/js/runtime-main.cd0dad9d.js"
+  }
+]);
